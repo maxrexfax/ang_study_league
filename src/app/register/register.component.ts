22ab6011e282857eaca2cd315d3305189b9523ca
Max Barannyk <maxrexfax@gmail.com>
2022-03-28 00:03:26 +0300

    Register form first name steps

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {User} from '../user';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,13 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  };
+
+  firstNameValidation: boolean | undefined;
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
 
   registerNewUser(): void {
-    alert("Register here...")
-  }
+    alert("Register here...");
+    console.log(this.user);
 
+}
 }
